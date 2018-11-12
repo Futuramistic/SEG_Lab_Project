6255ca3bc64fee8959bb81af278bfd6d8e33640e
mateusznowak98 <mateusz.michal.nowak98@gmail.com>
2018-11-12 09:35:09 +0000

    Initial Website structure

---
+++ SEG Website/navigation.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>The Computer Gaming Society</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+
+<body>
+
+        <ul style="font-size: 19; font-weight: 900; color:#fefbf0">The Computer Gaming Society
+				<li style = "float: right"><a href="Login.php">Login</a></li>
+				<li style = "float: right"><a href="Search.php">Search</a></li>
+				<li style = "float: right; border-bottom: solid 3px rgb(255,0,68)"><a href="Home.php">Home</a></li>
+				</ul>
