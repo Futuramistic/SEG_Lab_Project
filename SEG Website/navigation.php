7b1abf27014c22605f070895bbb1d1a34eaf0095
mateusznowak98 <mateusz.michal.nowak98@gmail.com>
2018-11-12 10:33:48 +0000

    Add Home page

--- SEG Website/navigation.php
+++ SEG Website/navigation.php
@@ -3,12 +3,14 @@
 <title>The Computer Gaming Society</title>
 	<link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
+<div class = "banner">
 <div class = "nav">
   <div class = "innernav">
     <div class="navtext">
       <li style="font-size: 19; font-weight: 900; color:#fefbf0">The Computer Gaming Society</li>
-      <li style = "float: right"><a href="#Login">My Account</a></li>
-      <li style = "float: right"><a href="#Search">Search</a></li>
-      <li style = "float: right; border-bottom: solid 3px #fffbfb">Home</li>
+      <li style = "float: right"><a href="Login.php">My Account</a></li>
+      <li style = "float: right"><a href="Search.php">Search</a></li>
+      <li style = "float: right"><a href="Home.php">Home</a></li>
   </div>
+</div>
 </div>
