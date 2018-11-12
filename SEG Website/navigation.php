d449fd16d9e91612acaa0179ea733055850ea63d
mateusznowak98 <mateusz.michal.nowak98@gmail.com>
2018-11-12 09:49:24 +0000

    Add CSS stylesheet

--- SEG Website/navigation.php
+++ SEG Website/navigation.php
@@ -1,13 +1,14 @@
 <html>
 <head>
 <title>The Computer Gaming Society</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
-
-<body>
-
-        <ul style="font-size: 19; font-weight: 900; color:#fefbf0">The Computer Gaming Society
-				<li style = "float: right"><a href="Login.php">Login</a></li>
-				<li style = "float: right"><a href="Search.php">Search</a></li>
-				<li style = "float: right; border-bottom: solid 3px rgb(255,0,68)"><a href="Home.php">Home</a></li>
-				</ul>
+<div class = "nav">
+  <div class = "innernav">
+    <div class="navtext">
+      <li style="font-size: 19; font-weight: 900; color:#fefbf0">The Computer Gaming Society</li>
+      <li style = "float: right"><a href="#Login">My Account</a></li>
+      <li style = "float: right"><a href="#Search">Search</a></li>
+      <li style = "float: right; border-bottom: solid 3px #fffbfb">Home</li>
+  </div>
+</div>
