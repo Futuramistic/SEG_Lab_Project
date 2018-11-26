0c9a40bf5b95941e5e1bc6240c8e8cf0415e8637
k1763930 <k1763930@kcl.ac.uk>
2018-11-26 09:36:38 +0000

    Add Database (Revised)

--- Lab-sql.sql
+++ Lab-sql.sql
@@ -8,7 +8,9 @@ TO 'admin'@'localhost'
 IDENTIFIED BY 'admin';
 
 CREATE TABLE Game (
-  id     	INT(50)		NOT NULL AUTO_INCREMENT,
+  id     	INT(50) 	AUTO_INCREMENT,
+  image VARCHAR(500),
+  review VARCHAR(500),
   name       	VARCHAR(50)	NOT NULL,
   platform     	VARCHAR(50)	NOT NULL,
   format        VARCHAR(50)	NOT NULL,
@@ -21,13 +23,15 @@ CREATE TABLE Game (
 );
 
 CREATE TABLE User (
-  userID     VARCHAR(50) UNIQUE NOT NULL,
+  userID     INT(50) 		AUTO_INCREMENT,
   firstName  VARCHAR(50)        NOT NULL,
   secondName VARCHAR(50)        NOT NULL,
-  password   VARCHAR(50)        NOT NULL,
+  password   VARCHAR(255)        NOT NULL,
   email      VARCHAR(50)        NOT NULL,
-  user_name  VARCHAR(50)        NOT NULL,
+  user_name  VARCHAR(50)        NOT NULL UNIQUE,
+  fees        FLOAT(10, 2),
   admin      SMALLINT(1)        NOT NULL,
+  administration SMALLINT(1)    NOT NULL,
   banned     SMALLINT(1)        NOT NULL,
   PRIMARY KEY (userID)
 );
@@ -37,9 +41,13 @@ CREATE TABLE Renting (
   rentDate DATETIME           NOT NULL,
   dueDate  DATETIME           NOT NULL,
   duration DATETIME           NOT NULL,
-  userid   VARCHAR(50),
-  gameid   VARCHAR(50),
-  FOREIGN KEY (gameid) REFERENCES Game (id),
-  FOREIGN KEY (userid) REFERENCES User (userID),
+  userid   INT(50)   NOT NULL,
+  gameid   INT(50)  NOT NULL,
+  extentions INT(2) DEFAULT '0.00',
+  FOREIGN KEY (gameid) REFERENCES Game(id),
+  FOREIGN KEY (userid) REFERENCES User(userID),
   PRIMARY KEY (id)
 );
+
+Insert Into User(firstName, secondName, password, email, user_name, admin, administration, banned)
+Values ('Admin','Admin','$2y$10$xkEed2jTNfDQOTZ5qm7W9uPIyL9xZsNCv6oFeIxqXcLkpyUJiY/V6','admin@gmail.com','admin','1', '1', '0');
