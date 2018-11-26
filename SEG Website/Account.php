f1b17717d2b520cf914d3b28c3212f8e0a0a79f3
Mustafa, Fahad (k1764029) <fahad.mustafa@kcl.ac.uk>
2018-11-26 09:41:19 +0000

    Add files via upload

---
+++ SEG Website/Account.php
@@ -0,0 +1,30 @@
+<?php require("navigation.php")?>
+<div class = "banner">
+    <div class = "bannerinner">
+      <div class = "bannercontent">
+      <h2>Account Settings</h2>
+			</div>
+	<style>		
+	h3 {
+    padding: 20px;
+    margin-right: 200px;
+    margin-left: 200px;
+    text-align: center;
+    }
+
+    h2{
+        text-align: center;
+    }
+    </style>
+	<h2>This is an overview of your account where you can check if you have any overdue items, outstanding fees, currently rented items and membership status.</h2>
+	<h3 style = "border: 1px solid black;">Your current membership status is: </h3>
+	<h3 style = "border: 1px solid black;">Here is a list of your current overdue items: </h3>
+	<h3 style = "border: 1px solid black;">Here are all the items that you are currently renting: </h3>
+	<h3 style = "border: 1px solid black;">Outstanding fees: </h3>
+
+</div>
+</div>
+
+
+</body>
+</html>
